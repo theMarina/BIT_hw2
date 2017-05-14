a0837ecb2b9205904463bb6c5e38686eac08e83c
Marina Minkin <mini@cs.technion.ac.il>
2017-05-14 17:29:43 +0300

    finally compiles

--- ex2.cpp
+++ ex2.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <set>
+#include <map>
 
 typedef std::pair<UINT32, unsigned int> func_t;
 struct more_func{ bool operator() (const func_t& x, const func_t& y)
@@ -16,48 +17,80 @@ typedef std::set<func_t, more_func> sorted_func_list_t;
 func_list_t g_func_list;
 func_names_t g_func_names;
 
-struct bbl_key_t
-{
-	ADDRINT bbl_addr;
-	USIZE bbl_size;
-};
+typedef std::pair<ADDRINT, USIZE> bbl_key_t;	// <ADDRINT bbl_addr, USIZE bbl_size>
 
 struct bbl_val_t
 {
 	unsigned long counter;	// #times this BBL was executed
-	string rtn_name;
-	INT32 rtn_id;	// this is just for runtime, for performance
+	/* const */ ADDRINT first_ins;
+	/* const */ string rtn_name;
+	/* const */ INT32 rtn_id;	// this is just for runtime, for performance
 				// because on each jump, the invoked BBL has
 				// to check if last instruction (from global
 				// variable) was from the same RTN
-	ADDRINT target_nt;	// target if branch is not taken.
+	/* const */ ADDRINT target_nt;	// target if branch is not taken.
 					// fill this value in instrumentation time
 	unsigned long counter_nt;
 	ADDRINT target_t;	// target if branch is taken. Fill this only AFTER branch was taken
 	unsigned long counter_t;
 };
 
-VOID CountBbl(UINT32 numInstInBbl, unsigned int *counter)
+struct bbl_val_t* g_last_bbl_val_ptr = NULL;	// this is the last BBL that was executeda
+
+std::map<bbl_key_t, bbl_val_t> g_bbl_map;
+
+VOID CountBbl(struct bbl_val_t* bbl_val_ptr)
 {
-	*counter += numInstInBbl;
+	(bbl_val_ptr->counter) ++;
+	if(!g_last_bbl_val_ptr)
+		goto out;
+	if(bbl_val_ptr->rtn_id != g_last_bbl_val_ptr->rtn_id)
+		goto out;	// the last bbl was a different function than the current bbl
+	if(bbl_val_ptr->first_ins == g_last_bbl_val_ptr->target_nt) {
+		(g_last_bbl_val_ptr->counter_nt) ++;
+		goto out;
+	}
+	g_last_bbl_val_ptr->target_t = bbl_val_ptr->first_ins;
+	(g_last_bbl_val_ptr->counter_t) ++;
+out:
+	g_last_bbl_val_ptr = bbl_val_ptr;
 }
+
 
 VOID Trace(TRACE trace, VOID *v)
 {
 	for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
 	{
-		RTN rtn = INS_Rtn(BBL_InsHead(bbl));
-		if (!RTN_Valid(rtn)) continue;
+		bbl_key_t bbl_key;
+		bbl_key.first = BBL_Address(bbl);
+		bbl_key.second = BBL_Size(bbl);
+		
+		struct bbl_val_t* bbl_val_ptr;
 
-		INT32 id = RTN_Id(rtn);
-		std::string &temp = g_func_names[id];
-		if (temp.empty()) temp = RTN_Name(rtn);
+		std::map<bbl_key_t, bbl_val_t>::iterator it = g_bbl_map.find(bbl_key);
+		if(it == g_bbl_map.end()) {	// creating a new entry in the map
+			struct bbl_val_t bbl_val;
+			INS first_ins = BBL_InsHead(bbl);
+			RTN rtn = INS_Rtn(first_ins);
+			if (!RTN_Valid(rtn)) continue;
+
+			bbl_val.counter = 0;
+			bbl_val.first_ins = INS_Address(first_ins);
+			bbl_val.rtn_id = RTN_Id(rtn);
+			bbl_val.rtn_name = RTN_Name(rtn);
+			bbl_val.target_nt = INS_Address(BBL_InsTail(bbl)) + INS_Size(BBL_InsTail(bbl)); // TODO:reduce InsTail calls
+			bbl_val.counter_nt = 0;
+			bbl_val.counter_t = 0;
+			it = g_bbl_map.insert(g_bbl_map.begin(), make_pair(bbl_key, bbl_val));
+		}
+		bbl_val_ptr = &(it->second);
+
 		
 		BBL_InsertCall(bbl,
 			IPOINT_ANYWHERE,
 			(AFUNPTR)CountBbl,
-			IARG_UINT32, BBL_NumIns(bbl),
-			IARG_PTR, &g_func_list[id],
+			IARG_PTR,
+			(void*)bbl_val_ptr,
 			IARG_END);
 	}
 }
