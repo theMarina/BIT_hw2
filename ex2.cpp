86eb353c77bb6e388cbb0f5c51f745fef28e2bdd
Gil Kupfer <gilkup@cs.technion.ac.il>
2017-05-16 00:00:27 +0300

    fixed instrumantations

--- ex2.cpp
+++ ex2.cpp
@@ -19,8 +19,8 @@ typedef std::set<func_t, more_func> sorted_func_list_t;
 func_list_t g_func_list;
 func_names_t g_func_names;
 
-typedef std::pair<ADDRINT, USIZE> bbl_key_t;	// <ADDRINT bbl_addr, USIZE bbl_size>
 
+typedef ADDRINT bbl_key_t;
 struct bbl_val_t
 {
 	unsigned long counter;	// #times this BBL was executed
@@ -32,79 +32,116 @@ struct bbl_val_t
 				// because on each jump, the invoked BBL has
 				// to check if last instruction (from global
 				// variable) was from the same RTN
-	/* const */ ADDRINT target_nt_addr;	// target if branch is not taken.
-					// fill this value in instrumentation time
-	bbl_val_t* target_nt;	// target if branch is NOT taken. Fill this only AFTER branch was taken
-	unsigned long counter_nt;
-	bbl_val_t* target_t;	// target if branch is taken. Fill this only AFTER branch was taken
-	unsigned long counter_t;
+	/* const */ UINT32 size;
+
+	ADDRINT target[2]; // taken/not taken
+	unsigned int target_count[2];
 
 	int idx_for_printing;	// used only for printing
 };
 
-struct bbl_val_t* g_last_bbl_val_ptr = NULL;	// this is the last BBL that was executeda
-
 std::map<bbl_key_t, bbl_val_t> g_bbl_map;
 
-VOID CountBbl(struct bbl_val_t* bbl_val_ptr)
+VOID bbl_count(struct bbl_val_t* bbl_val_ptr)
 {
-	(bbl_val_ptr->counter) ++;
-	if(!g_last_bbl_val_ptr)
-		goto out;
-	if(bbl_val_ptr->rtn_id != g_last_bbl_val_ptr->rtn_id)
-		goto out;	// the last bbl was a different function than the current bbl
-	if(bbl_val_ptr->first_ins == g_last_bbl_val_ptr->target_nt_addr) {
-		g_last_bbl_val_ptr->target_nt = bbl_val_ptr;
-		(g_last_bbl_val_ptr->counter_nt) ++;
-		goto out;
-	}
-	g_last_bbl_val_ptr->target_t = bbl_val_ptr;
-	(g_last_bbl_val_ptr->counter_t) ++;
-out:
-	g_last_bbl_val_ptr = bbl_val_ptr;
+	++bbl_val_ptr->counter;
 }
 
+VOID direct_edge_count(struct bbl_val_t* bbl_val_ptr, INT32 isTaken, ADDRINT fallthroughAddr, ADDRINT takenAddr)
+{
+	bbl_val_ptr->target[0] = fallthroughAddr;
+	bbl_val_ptr->target[1] = takenAddr;
+	//std::cout << "gilkup: " << std::hex << fallthroughAddr << "/" << takenAddr << std::dec << " Taken:" << isTaken <<  std::endl;
+	++bbl_val_ptr->target_count[!!isTaken];
+}
+
+VOID fallthrough_edge_count(struct bbl_val_t* bbl_val_ptr, INT32 isTaken, ADDRINT fallthroughAddr)
+{
+	if (!isTaken)
+	{
+		bbl_val_ptr->target[0] = fallthroughAddr;
+		++bbl_val_ptr->target_count[0];
+	}
+}
 
 VOID Trace(TRACE trace, VOID *v)
 {
 	for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
 	{
-		bbl_key_t bbl_key;
-		bbl_key.first = BBL_Address(bbl);
-		bbl_key.second = BBL_Size(bbl);
+		bbl_key_t bbl_key = BBL_Address(bbl);
+
+		INS first_ins = BBL_InsHead(bbl);
+		INS last_ins = BBL_InsTail(bbl);
 		
-		struct bbl_val_t* bbl_val_ptr;
+		RTN rtn = INS_Rtn(first_ins);
+		if (!RTN_Valid(rtn)) continue;
 
 		std::map<bbl_key_t, bbl_val_t>::iterator it = g_bbl_map.find(bbl_key);
 		if(it == g_bbl_map.end()) {	// creating a new entry in the map
 			struct bbl_val_t bbl_val;
-			INS first_ins = BBL_InsHead(bbl);
-			INS last_ins = BBL_InsTail(bbl);
-			RTN rtn = INS_Rtn(first_ins);
-			if (!RTN_Valid(rtn)) continue;
 
 			bbl_val.counter = 0;
+			bbl_val.size = BBL_Size(bbl);
 			bbl_val.first_ins = INS_Address(first_ins);
 			bbl_val.last_ins = INS_Address(last_ins);
 			bbl_val.rtn_id = RTN_Id(rtn);
 			bbl_val.rtn_name = RTN_Name(rtn);
 			bbl_val.rtn_addr = RTN_Address(rtn);
-			bbl_val.target_nt_addr = bbl_val.last_ins + INS_Size(last_ins);
-			bbl_val.target_nt = NULL;
-			bbl_val.counter_nt = 0;
-			bbl_val.target_t = NULL;
-			bbl_val.counter_t = 0;
+			bbl_val.target[0] = bbl_val.target[1] = 0;
+			bbl_val.target_count[0] = bbl_val.target_count[1] = 0;
+
 			it = g_bbl_map.insert(g_bbl_map.begin(), make_pair(bbl_key, bbl_val));
 		}
-		bbl_val_ptr = &(it->second);
-
 		
+		struct bbl_val_t* bbl_val_ptr = &(it->second);
+
+		//std::cout << std::hex << bbl_val_ptr->last_ins << std::dec << ": " << INS_Disassemble(last_ins) << std::endl;
+
 		BBL_InsertCall(bbl,
-			IPOINT_ANYWHERE,
-			(AFUNPTR)CountBbl,
-			IARG_PTR,
-			(void*)bbl_val_ptr,
+			IPOINT_BEFORE,
+			(AFUNPTR)bbl_count,
+			IARG_PTR, (void*)bbl_val_ptr,
 			IARG_END);
+
+		if(INS_IsDirectBranch(last_ins))
+			INS_InsertCall(last_ins,
+				IPOINT_BEFORE,
+				(AFUNPTR)direct_edge_count,
+				IARG_PTR, (void*)bbl_val_ptr,
+				IARG_BRANCH_TAKEN,
+				IARG_FALLTHROUGH_ADDR,
+				IARG_BRANCH_TARGET_ADDR,
+				IARG_END);
+
+		else if(INS_IsBranchOrCall(last_ins))
+			INS_InsertCall(last_ins,
+				IPOINT_BEFORE,
+				(AFUNPTR)fallthrough_edge_count,
+				IARG_PTR, (void*)bbl_val_ptr,
+				IARG_BRANCH_TAKEN,
+				IARG_FALLTHROUGH_ADDR,
+				IARG_END);
+
+		else if (INS_HasRealRep(last_ins))
+		/*
+			INS_InsertCall(last_ins,
+				IPOINT_AFTER,
+				(AFUNPTR)direct_edge_count,
+				IARG_PTR, (void*)bbl_val_ptr,
+				IARG_BRANCH_TAKEN,
+				IARG_FALLTHROUGH_ADDR,
+				IARG_INST_PTR,
+				IARG_END);
+		*/;
+
+		else
+			INS_InsertCall(last_ins,
+				IPOINT_BEFORE,
+				(AFUNPTR)fallthrough_edge_count,
+				IARG_PTR, (void*)bbl_val_ptr,
+				IARG_BOOL, 0,
+				IARG_FALLTHROUGH_ADDR,
+				IARG_END);
 	}
 }
 
@@ -159,23 +196,30 @@ VOID Fini(INT32 code, VOID *v)
 			printing_rtn.rtn_addr = it->second.rtn_addr;
 			print_it = printing_ds.insert(printing_ds.begin(), make_pair(it->second.rtn_id, printing_rtn));
 		}
-		print_it->second.counter += (it->first.second) * (it->second.counter);
+		print_it->second.counter += (it->second.size * it->second.counter);
 		print_it->second.printing_bbl_list.push_back(&(it->second));
-		// TODO: make sure I don't print edges with count 0
-		printing_edge_t edge_nt, edge_t;
 
-		edge_nt.edge_from = &(it->second);
-		edge_nt.edge_to = it->second.target_nt;
-		edge_nt.edge_count = it->second.counter_nt;
+		if (it->second.target_count[0])
+		{
+			printing_edge_t edge;
+			edge.edge_from = &(it->second);
+			edge.edge_to = &g_bbl_map[it->second.target[0]];
+			edge.edge_count = it->second.target_count[0];
+			if (edge.edge_from->rtn_id == edge.edge_to->rtn_id)
+				print_it->second.printing_edges.push_back(edge);
+			//std::cout << "0 from: " << std::hex << edge.edge_from->first_ins << " to: " << edge.edge_to->first_ins << std::dec << std::endl;
+		}
 
-		edge_t.edge_from = &(it->second);
-		edge_t.edge_to = it->second.target_t;
-		edge_t.edge_count = it->second.counter_t;
-
-		if(edge_nt.edge_from && edge_nt.edge_to && edge_nt.edge_count)
-			print_it->second.printing_edges.push_back(edge_nt);
-		if(edge_t.edge_from && edge_t.edge_to && edge_t.edge_count)
-			print_it->second.printing_edges.push_back(edge_t);
+		if (it->second.target_count[1])
+		{
+			printing_edge_t edge;
+			edge.edge_from = &(it->second);
+			edge.edge_to = &g_bbl_map[it->second.target[1]];
+			edge.edge_count = it->second.target_count[1];
+			if (edge.edge_from->rtn_id == edge.edge_to->rtn_id)
+				print_it->second.printing_edges.push_back(edge);
+			//std::cout << "1 from: " << std::hex << edge.edge_from->first_ins << " to: " << edge.edge_to->first_ins << std::dec << std::endl;
+		}
 	}
 
 	for(std::map<INT32, printing_rtn_t>::iterator print_it = printing_ds.begin() ; print_it != printing_ds.end() ; ++print_it) { // TODO: this is not sorted by the counter
